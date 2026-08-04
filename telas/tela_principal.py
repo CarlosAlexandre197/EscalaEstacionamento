903cb4ac07f5a58a92cbfcef0b930ec1cb4f3d4e
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2026-08-04 06:57:53 -0300

    Atualizar o tela_principal.py
    Ainda ligando a janela ao botão obreiros

--- telas/tela_principal.py
+++ telas/tela_principal.py
@@ -259,6 +259,8 @@ class TelaPrincipal(QWidget):
 
         self.btn_adicionar.clicked.connect(self.adicionar_culto)
         self.btn_remover.clicked.connect(self.remover_culto)
+
+self.btn_obreiros.clicked.connect(self.abrir_cadastro_obreiros)
         
     def adicionar_culto(self):
     
