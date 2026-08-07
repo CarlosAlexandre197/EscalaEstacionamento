3aa2e4e6b27220ac382ccea0284e8ff2c23e17fa
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2026-08-07 17:49:33 -0300

    Atualizar o tela_principal.py
    Tirando o self.carregar_escala de dentro do for

--- telas/tela_principal.py
+++ telas/tela_principal.py
@@ -223,7 +223,7 @@ class TelaPrincipal(QWidget):
                 3,
                 combo_obreiro
             )
-            self.carregar_escala()
+self.carregar_escala()
             
     def criar_layout(self):
 
