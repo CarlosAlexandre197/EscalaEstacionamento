d14ce992bf6d96366463e2c9c793c3c83c136943
Carlos Alexandre <alexandrec191@gmail.com>
2026-08-11 12:50:10 -0300

    Alterando o código do whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,11 +1,24 @@
 import webbrowser
+import os
 
 
 def abrir_whatsapp():
-    """
-    Abre o WhatsApp Web no navegador.
-    """
 
     webbrowser.open(
         "https://web.whatsapp.com/"
     )
+
+
+def enviar_pdf_whatsapp(caminho_pdf):
+
+    if not caminho_pdf:
+        return False
+
+    if not os.path.exists(caminho_pdf):
+        return False
+
+    webbrowser.open(
+        "https://web.whatsapp.com/"
+    )
+
+    return True
